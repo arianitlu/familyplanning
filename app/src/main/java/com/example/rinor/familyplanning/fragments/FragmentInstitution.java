045c6ab05a1745d0545ccaade18db162783c4e4b
Arianit Lubishtani <arianit.lu@hotmail.com>
2019-01-31 11:16:41 +0100

    Dialog fixed when an institution is clicked

--- app/src/main/java/com/example/rinor/familyplanning/fragments/FragmentInstitution.java
+++ app/src/main/java/com/example/rinor/familyplanning/fragments/FragmentInstitution.java
@@ -2,6 +2,7 @@ package com.example.rinor.familyplanning.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -12,7 +13,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -37,9 +40,10 @@ import java.util.List;
 public class FragmentInstitution extends Fragment {
 
     private static final String FAMILY_PLANNING_BASE_URL = "http://192.168.0.169/familyplanning/readInstitutions.php";
+
     RecyclerView institutionRecyclerView;
     InstitutionAdapter adapter;
-    List<Institution> list = new ArrayList<>();
+
     List<Institution> institutionList = new ArrayList<>();
 
     InstitutionAdapter.InstitutionClickHandler clickHandler;
@@ -53,24 +57,36 @@ public class FragmentInstitution extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_instution, container, false);
 
-        myDialog = new Dialog(getActivity());
+        myDialog = new Dialog(getContext());
+        myDialog.setContentView(R.layout.popup_institution_name);
+
+        txtname = myDialog.findViewById(R.id.txtPopUpName);
+        txtDescription = myDialog.findViewById(R.id.txtPopUpDescription);
+        txtServices = myDialog.findViewById(R.id.txtPopUpServices);
+        txtWeb = myDialog.findViewById(R.id.txtPopupWeb);
 
         institutionRecyclerView = view.findViewById(R.id.recyclerview_instutiton);
 
-        getInstitutionList(clickHandler);
+        getInstitutionList();
 
         myDialog.setCanceledOnTouchOutside(true);
 
+        institutionRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getContext(), institutionRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        getInstitutionListById(position);
+                        myDialog.show();
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                    }
+                })
+        );
 
         return view;
     }
 
-    public void fillData(Institution institution) {
-        txtname.setText(institution.getName());
-        txtDescription.setText(institution.getDescription());
-    }
-
-    public void getInstitutionList(final InstitutionAdapter.InstitutionClickHandler clickHandler) {
+    public void getInstitutionList() {
 
         Uri baseUri = Uri.parse(FAMILY_PLANNING_BASE_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
@@ -83,10 +99,12 @@ public class FragmentInstitution extends Fragment {
                         try {
                             institutionList = JsonUtil.extractAllInstitutions(response);
                             institutionRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
                             adapter = new InstitutionAdapter(institutionList);
-
                             institutionRecyclerView.setAdapter(adapter);
+
+                            String name = institutionList.get(0).getName();
+                            txtname.setText(name);
+                            txtDescription.setText(name);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -102,6 +120,87 @@ public class FragmentInstitution extends Fragment {
                 });
 
         MySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    public void getInstitutionListById(final int id) {
+
+        Uri baseUri = Uri.parse(FAMILY_PLANNING_BASE_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, uriBuilder.toString(), null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            institutionList = JsonUtil.extractAllInstitutions(response);
+                            institutionRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                            adapter = new InstitutionAdapter(institutionList);
+                            institutionRecyclerView.setAdapter(adapter);
+
+                            txtname.setText(institutionList.get(id).getName());
+                            txtDescription.setText(institutionList.get(id).getDescription());
+                            txtServices.setText(institutionList.get(id).getServices());
+                            txtWeb.setText(institutionList.get(id).getWebsite());
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        MySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    public static class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+        private OnItemClickListener mListener;
+
+        public interface OnItemClickListener {
+            public void onItemClick(View view, int position);
+
+            public void onLongItemClick(View view, int position);
+        }
+
+        GestureDetector mGestureDetector;
+
+        public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener) {
+            mListener = listener;
+            mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && mListener != null) {
+                        mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+            View childView = view.findChildViewUnder(e.getX(), e.getY());
+            if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+                mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
+                return true;
+            }
+            return false;
+        }
+
+        @Override public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) { }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent (boolean disallowIntercept){}
     }
 }
 
